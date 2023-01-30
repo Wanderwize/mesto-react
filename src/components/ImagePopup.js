12e35a93e6ed759a08575378f808fd15e1de0280
Wanderwize~ <wanderwwwize@yandex.ru>
2023-01-30 11:47:33 +0500

    sprint 11 iter 5

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -6,18 +6,18 @@ function ImagePopup({ card, onClose }) {
         id="image-popup"
         style={{ backgroundColor: "rgba(0, 0, 0, 0.9)" }}
       >
-        <div className="popup-image__container">
+        <div className="popup__image_container">
           <button
             onClick={onClose}
             type="button"
-            className="popup__close popup-image__close"
+            className="popup__close popup__image_close"
           ></button>
           <img
             src={`${card ? card.link : ""}`}
-            className="popup-image__window"
+            className="popup__image_window"
             alt={`${card ? card.name : ""}`}
           />
-          <h2 className="popup-image__subtitle">{`${
+          <h2 className="popup__image_subtitle">{`${
             card ? card.name : ""
           }`}</h2>
         </div>
